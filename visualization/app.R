fc66f74b34aa44102c6aa6ae6076a9ccbc586931
Alan Peral <alanperal19@gmail.com>
2017-12-08 13:43:13 -0800

    slider working to select year. country selector works but only for one country at a time (meaning you have to delete country before choosing another one). indicator checkbox does not work yet, only displays fertility rate

--- visualization/app.R
+++ visualization/app.R
@@ -8,7 +8,7 @@
 #
 
 library(shiny)
-
+library(ggplot2)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -19,13 +19,27 @@ ui <- fluidPage(
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30),
-      selectizeInput('text','Country selector', choices = country.name, multiple = TRUE)
+      sliderInput("range", "Range:",
+                  min = 1960, max = 2015,
+                  value = c(1960,2015)),
+      hr(),
+      
+      fluidRow(
+        column(4, verbatimTextOutput("range"))
+      ),
+      selectizeInput('text','Country selector', choices = country.name, multiple = TRUE, selected = "Afghanistan"),
+      checkboxGroupInput("checkGroup", label = ("Indicators"), 
+                         choices = list("Health Expenditure" = 1, "Fertility Rate" = 2, "Life Expectancy Female" = 3, "Mortality Rate Under 5" = 4, 
+                                        "Children Employment" = 5, "Labor Force Female" = 6, "Labor Force Participation Rate" = 7, 
+                                        "Gini Index" = 7, "Refugee Origin" = 8, "Refugee Asylum" = 9, "Sanitation Access" = 10,
+                                        "Water Access" = 11, "Electricity Access" = 12, "Slums Population" = 13),
+                         selected = 1),
+      
+      
+      hr(),
+      fluidRow(column(3, verbatimTextOutput("value")))
     ),
+    
     
     # Show a plot of the generated distribution
     mainPanel(
@@ -41,12 +55,25 @@ server <- function(input, output) {
     # generate bins based on input$bins from ui.R
     x    <- WBD.SES.conflict[, 30] # fertility rate
     x    <- na.omit(x)
-    x 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    year.range <- { input$range }
+    lower <- year.range[1]
+    upper <- year.range[2]
+   
+    country <- input$text
+    indicator.vector <- na.omit(WBD.SES.conflict[which(WBD.SES.conflict$location == country), c("fetility_rate", "year")])
+    # only display the years in range
+    indicator.vector <- indicator.vector[which(indicator.vector$year >= lower & indicator.vector$year <= upper),]
     
-    # draw the histogram with the specified number of bins
-    hist(x, main = paste("Histogram of fertility rates"), xlab = "Fertiliy Rate", breaks = bins, col = 'darkgray', border = 'white')
+    # draw the line graph with the specified number of bins
+    ggplot(indicator.vector, aes(x = indicator.vector$year)) + geom_line(aes(y=indicator.vector$fetility_rate)) + xlab("Year") + ylab("fertility rate")
+    #hist(x, main = paste("Histogram of fertility rates"), xlab = "Fertiliy Rate", breaks = bins, col = 'darkgray', border = 'white')
   })
+  
+  # output$linePlot <- renderPlot({
+  #   ctry <- input$text
+  #   y    <- WBD.SES.conflict$ctry
+  #   y
+  # })
 }
 
 # Run the application 
